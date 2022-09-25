d2c5cd58515c17e413bd88dc6f6d7f6e21fff2c4
hayk-simonyan <hayksimonyan7@gmail.com>
2022-09-25 18:56:47 +0400

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.NODE_LOCAL_PORT);
 }
 bootstrap();
