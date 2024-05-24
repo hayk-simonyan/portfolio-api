5d68f1681761fb9c32221bb6af76bc09dc3760d5
hayk-simonyan <hayksimonyan7@gmail.com>
2024-05-24 14:19:08 +0400

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'https://hayksimonyandev.web.app/',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
-
+  app.enableCors();
   await app.listen(process.env.PORT);
 }
 bootstrap();
